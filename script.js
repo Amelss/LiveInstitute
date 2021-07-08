d8973c21a398918f2146c9b1642fab9791e91d45
Amelss <ameleyka@gmail.com>
2021-07-08 10:06:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let authorName = 'Maya Angelou';
+let nameOfBook= 'Yellow Tail';
+let favouriteParagraph = `"So long as there is rain in the world, there is still hope." - FD`;
+let longString = `From the book,
+I learned that as long as there
+is still rain in the world there is still hope.
+This means that as long as there is water we are not
+doomed becuase we will only be doomed
+when there is no more water.`
+let longerString = "I learned that " + "+ as long as there is rain " + "+ in the world there is still hope"
+console.log(authorName);
+console.log(nameOfBook);
+console.log(favouriteParagraph);
+console.log(longString);
+console.log(longerString)
